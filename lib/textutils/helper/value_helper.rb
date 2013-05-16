ee0619b213c1b594191b53f9e2869730939677ba
Gerald Bauer <gerald.bauer@gmail.com>
2013-05-16 11:09:50 +0200

    cleanup value reader

--- lib/textutils/helper/value_helper.rb
+++ lib/textutils/helper/value_helper.rb
@@ -34,10 +34,10 @@ module TextUtils
   end
 
 
-  def find_grade( text )  # NB: returns ary [grade,text] / two values
+  def find_grade( value )  # NB: returns ary [grade,value] / two values
     grade = 4  # defaults to grade 4  e.g  *** => 1, ** => 2, * => 3, -/- => 4
 
-    text = text.sub( /\s+(\*{1,3})\s*$/ ) do |_|  # NB: stars must end field/value
+    value = value.sub( /\s+(\*{1,3})\s*$/ ) do |_|  # NB: stars must end field/value
       if $1 == '***'
         grade = 1
       elsif $1 == '**'
@@ -50,10 +50,66 @@ module TextUtils
       ''  # remove * from title if found
     end
 
-    [grade,text]
+    [grade,value]
   end
 
 
+  def find_key_n_title( values )  # NB: returns ary [attribs,more_values] / two values
+
+    ## fix: add/configure logger for ActiveRecord!!!
+    logger = LogKernel::Logger.root
+
+    ### todo/fix: allow check - do NOT allow mixed use of with key and w/o key
+    ##  either use keys or do NOT use keys; do NOT mix in a single fixture file
+
+    ### support autogenerate key from first title value
+
+    # if it looks like a key (only a-z lower case allowed); assume it's a key
+    #   - also allow . in keys e.g. world.quali.america, at.cup, etc.
+    #   - also allow 0-9 in keys e.g. at.2, at.3.1, etc.
+
+    # fix/todo: add support for leading underscore _
+    #   or allow keys starting w/ digits?
+    if values[0] =~ /^([a-z][a-z0-9.]*[a-z0-9]|[a-z])$/    # NB: key must start w/ a-z letter (NB: minimum one letter possible)
+      key_col         = values[0]
+      title_col       = values[1]
+      more_values     = values[2..-1]
+    else
+      key_col         = '<auto>'
+      title_col       = values[0]
+      more_values     = values[1..-1]
+    end
+
+    attribs = {}
+
+    ## check title_col for grade (e.g. ***/**/*) and use returned stripped title_col if exits
+    grade, title_col = find_grade( title_col )
+
+    # NB: for now - do NOT include default grade e.g. if grade (***/**/*) not present; attrib will not be present too
+    if grade == 1 || grade == 2 || grade == 3  # grade found/present
+      logger.debug "   found grade #{grade} in title"
+      attribs[:grade] = grade
+    end
+
+    ## title (split of optional synonyms)
+    # e.g. FC Bayern Muenchen|Bayern Muenchen|Bayern
+    titles = title_col.split('|')
+
+    attribs[ :title ]    =  titles[0]
+
+    ## add optional synonyms if present
+    attribs[ :synonyms ] =  titles[1..-1].join('|')  if titles.size > 1
+
+    if key_col == '<auto>'
+      ## autogenerate key from first title
+      key_col = TextUtils.title_to_key( titles[0] )
+      logger.debug "   autogen key »#{key_col}« from title »#{titles[0]}«"
+    end
+
+    attribs[ :key ] = key_col
+
+    [attribs, more_values]
+  end
 
   end # module ValueHelper
 end # module TextUtils
--- lib/textutils/reader/values_reader.rb
+++ lib/textutils/reader/values_reader.rb
@@ -6,13 +6,13 @@ class ValuesReader
 
   include LogUtils::Logging
   
-  include TextUtils::ValueHelper # e.g. includes find_grade()
+  include TextUtils::ValueHelper # e.g. includes find_grade, find_key_n_title
 
 
-  def initialize( path, more_values={} )
+  def initialize( path, more_attribs={} )
     @path = path
 
-    @more_values = more_values
+    @more_attribs = more_attribs
 
     @data = File.read_utf8( @path )
   end
@@ -29,8 +29,8 @@ class ValuesReader
   def each_line   # support multi line records
 
     inside_line = false   # todo: find a better name? e.g. line_found?
-    attribs = {}     # rename to new_attributes?
-    more_cols = []   # rename to more_values?
+    attribs = {}
+    more_values = []
       
 
     @data.each_line do |line|
@@ -73,15 +73,15 @@ class ValuesReader
          #  allows you to use any chars
          logger.debug "   multi-line record - add key-value »#{line}«"
 
-         more_cols.unshift( line.dup )   # add value upfront to array (first value); lets us keep (optional) tags as last entry; fix!! see valuereaderEx v2
+         more_values.unshift( line.dup )   # add value upfront to array (first value); lets us keep (optional) tags as last entry; fix!! see valuereaderEx v2
          next
       else
         # NB: new record clears/ends multi-line record
-        
+
         if inside_line  # check if we already processed a line? if yes; yield last line
-          yield( attribs, more_cols )
-          attribs   = {}
-          more_cols = []
+          yield( attribs, more_values )
+          attribs     = {}
+          more_values = []
         end
         inside_line = true
       end
@@ -115,67 +115,18 @@ class ValuesReader
           true
         end
       end
-      
+
       logger.debug "  values: »#{values.join('« »')}«"
-      
-      
-      ### todo/fix: allow check - do NOT allow mixed use of with key and w/o key
-      ##  either use keys or do NOT use keys; do NOT mix in a single fixture file
-      
-      
-      ### support autogenerate key from first title value
-      
-      # if it looks like a key (only a-z lower case allowed); assume it's a key
-      #   - also allow . in keys e.g. world.quali.america, at.cup, etc.
-      #   - also allow 0-9 in keys e.g. at.2, at.3.1, etc.
 
-      # fix/todo: add support for leading underscore _
-      #   or allow keys starting w/ digits?
-      if values[0] =~ /^([a-z][a-z0-9.]*[a-z0-9]|[a-z])$/    # NB: key must start w/ a-z letter (NB: minimum one letter possible)
-        key_col         = values[0]
-        title_col       = values[1]
-        more_cols       = values[2..-1]
-      else
-        key_col         = '<auto>'
-        title_col       = values[0]
-        more_cols       = values[1..-1]
-      end
+      attribs, more_values = find_key_n_title( values )
 
-      attribs = {}
+      attribs = attribs.merge( @more_attribs )  # e.g. merge country_id and other defaults if present
 
-      ## check title_col for grade (e.g. ***/**/*) and use returned stripped title_col if exits
-      grade, title_col = find_grade( title_col )
-
-      # NB: for now - do NOT include default grade e.g. if grade (***/**/*) not present; attrib will not be present too
-      if grade == 1 || grade == 2 || grade == 3  # grade found/present
-        logger.debug "   found grade #{grade} in title"
-        attribs[:grade] = grade
-      end
-
-      ## title (split of optional synonyms)
-      # e.g. FC Bayern Muenchen|Bayern Muenchen|Bayern
-      titles = title_col.split('|')
-      
-      attribs[ :title ]    =  titles[0]
-     
-      ## add optional synonyms if present
-      attribs[ :synonyms ] =  titles[1..-1].join('|')  if titles.size > 1
-      
-      if key_col == '<auto>'
-        ## autogenerate key from first title
-        key_col = TextUtils.title_to_key( titles[0] )
-        logger.debug "   autogen key »#{key_col}« from title »#{titles[0]}«, textutils version #{TextUtils::VERSION}"
-      end
-      
-      attribs[ :key ] = key_col
-      
-      attribs = attribs.merge( @more_values )  # e.g. merge country_id and other defaults if present
-                        
     end # each lines
 
     # do NOT forget to yield last line (if present/processed)
     if inside_line
-      yield( attribs, more_cols )
+      yield( attribs, more_values )
     end
 
 
