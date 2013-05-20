a870e1bf8008ee3864a943dd5067eeec4e19dd59
Gerald Bauer <gerald.bauer@gmail.com>
2013-05-20 15:51:13 +0200

    add more matchers

--- lib/textutils/helper/value_helper.rb
+++ lib/textutils/helper/value_helper.rb
@@ -160,6 +160,44 @@ module TextUtils
   end
 
 
+  def match_abv( value )  # alcohol by volume (abv) e.g. 5.2% 
+    if value =~ /^<?\s*(\d+(?:\.\d+)?)\s*%$/
+      # nb: allow leading < e.g. <0.5%
+      yield( $1.to_f )  # convert to decimal? how? use float?
+      true # bingo - match found
+    else
+      false # no match found
+    end
+  end
+
+  def match_og( value ) # plato (stammwuerze/gravity?) e.g. 11.2°
+    if value =~ /^(\d+(?:\.\d+)?)°$/
+      # nb: no whitespace allowed between ° and number e.g. 11.2°
+      yield( $1.to_f )  # convert to decimal? how? use float?
+      true # bingo - match found
+    else
+      false # no match found
+    end
+  end
+
+  def match_kcal( value )
+    if value =~ /^(\d+(?:\.\d+)?)\s*kcal(?:\/100ml)?$/  # kcal
+      # nb: allow 44.4 kcal/100ml or 44.4 kcal or 44.4kcal
+      yield( $1.to_f )  # convert to decimal? how? use float?
+      true # bingo - match found
+    else
+      false # no match found
+    end
+  end
+
+  def match_hl( value )  # hector liters (hl) 1hl = 100l
+    if value =~ /^(?:([0-9][0-9_ ]+[0-9]|[0-9]{1,2})\s*hl)$/  # e.g. 20_000 hl or 50hl etc.
+      yield( $1.gsub( /[ _]/, '' ).to_i )
+      true # bingo - match found
+    else
+      false # no match found
+    end
+  end
 
 
   def is_website?( value )
--- lib/textutils/version.rb
+++ lib/textutils/version.rb
@@ -1,6 +1,6 @@
 
 module TextUtils
 
-  VERSION = '0.6.1'
+  VERSION = '0.6.2'
 
 end   # module TextUtils
